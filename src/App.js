20e2168b27d14145a3a268b9da2e2f93cbbfea05
Nikola Cucukovic <c-nikola@hotmail.com>
2023-01-04 19:27:21 +0100

    #19: Design Portfolio Section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ const App = () => {
     'technologies': {
       'title': 'Technologies',
       'description': 'Our technologies section highlights the cutting-edge tools and technologies we employ in our web design process.'
+    },
+    'portfolio': {
+      'title': 'Portfolio',
+      'description': 'Our portfolio section showcases our best web design projects and allows visitors to see our skills and experience.'
+    },
+    'contact': {
+      'title': 'Contact',
+      'description': 'Our contact section provides all the necessary information and a contact form for potential clients to get in touch with us.'
+    },
+    'blog': {
+      'title': 'Blog',
+      'description': 'Our blog section features regular updates and insights on web design trends, news, and best practices.'
     }
   };
 
@@ -16,6 +28,7 @@ const App = () => {
       <Header />
       <Hero />
       <Section title={content.technologies.title} description={content.technologies.description} />
+      <Section title={content.portfolio.title} description={content.portfolio.description} />
     </div>
   );
 }
